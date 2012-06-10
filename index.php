ec1ed8639a0b544d1f76f9d10ec3f70b0ccd821f
Azuka Okuleye <azuka@zatechcorp.com>
2012-06-10 18:09:42 -0400

    Initial layout

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Teaching</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div id="wrap">
+			<header>
+				<h1>Teaching</h1>
+				<nav>
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">News</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</nav>
+			</header>
+			<div id="main">
+				Content
+			</div>
+			<footer>
+				Footer content. Copyright &copy; Ernesto, Marvim &amp; Azuka <?php echo date('Y'); ?>
+			</footer>
+		</div>
+	</body>
+</html>
